0d041d71ef558daefa0444920e1b7b0a034875c3
Nicolas Malbran <nmalbran@gmail.com>
2013-07-09 19:19:22 -0400

    Added menu.

--- vote/urls.py
+++ vote/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 from views import StatsView, AppraiseView
 
 urlpatterns = patterns('',
+    url(r'^/?$', RedirectView.as_view(url='/stats/')),
     url(r'^appraise/?$', AppraiseView.as_view(), name='appraise'),
     url(r'^stats/?$', StatsView.as_view(), name='stats'),
 
